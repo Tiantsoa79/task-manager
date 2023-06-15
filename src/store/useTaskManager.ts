6f2ac532417b113169c0f44882c464a7d2beeffc
Tiantsoa79 <103513020+Tiantsoa79@users.noreply.github.com>
2023-06-15 11:39:57 +0300

    Update useTaskManager.ts

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,33 @@
-const useTaskManager = () => {
+import create from 'zustand';
 
-}
+const useTaskManager = create((set) => ({
+  tasks: [],
 
-export {
-  useTaskManager
-}
+  searchTask: (searchTerm) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) =>
+        task.title.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    }));
+  },
+
+  addTask: (newTask) => {
+    set((state) => ({ tasks: [...state.tasks, newTask] }));
+  },
+
+  updateTask: (taskId, updatedTask) => {
+    set((state) => ({
+      tasks: state.tasks.map((task) =>
+        task.id === taskId ? { ...task, ...updatedTask } : task
+      ),
+    }));
+  },
+
+  deleteTask: (taskId) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== taskId),
+    }));
+  },
+}));
+
+export { useTaskManager };
