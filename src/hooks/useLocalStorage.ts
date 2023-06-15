ab1d24c9087a50b696ffca488a6c9ba727d5d0d8
Tiantsoa79 <103513020+Tiantsoa79@users.noreply.github.com>
2023-06-15 11:29:34 +0300

    Update useLocalStorage.ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,33 @@
-const useLocalStorage = () => {
+import create from 'zustand';
 
-}
+const useTaskManager = create((set) => ({
+  tasks: [],
 
-export {
-  useLocalStorage
-}
+  searchTask: (searchTerm) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) =>
+        task.title.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    }));
+  },
+
+  addTask: (newTask) => {
+    set((state) => ({ tasks: [...state.tasks, newTask] }));
+  },
+
+  updateTask: (taskId, updatedTask) => {
+    set((state) => ({
+      tasks: state.tasks.map((task) =>
+        task.id === taskId ? { ...task, ...updatedTask } : task
+      ),
+    }));
+  },
+
+  deleteTask: (taskId) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== taskId),
+    }));
+  },
+}));
+
+export { useTaskManager };
